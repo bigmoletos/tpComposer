8afef81bbc80544d1bae667cab5dc7acbb4c4a21
franck <bigmoletos@yopmail.com>
2017-10-16 16:59:06 +0200

    Merge origin/aliou
    Conflicts:
    	repertoire _fanny/fichier1.txt
    	repertoire franck/fichier3.txt
    	repertoire isa/fichier4.txt
    	repertoire_isa/fichier4.txt

--- TP2/ValidatePass.php
+++ TP2/ValidatePass.php
@@ -4,12 +4,13 @@ class ValidatePass{
     const MINLGT = 6;
     const MAXLGT = 20;
     public function  ValidLength ($pass){
-        $passlength >= self::MINLGT && $passlength <= self::MAXLGT){
+        $passlength = strlen($pass);
+       if($passlength >= self::MINLGT && $passlength <= self::MAXLGT){
             return true;
         }else {
             return false;
         }
     }
   }
-}
+
 
